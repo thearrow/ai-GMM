6462f890ac78c9486e63003f28fd7d0aa0cf98a1
jake <thearrow3456@gmail.com>
2013-03-12 15:24:50 -0400

    data input

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Double> data = readData("data1.txt");
+        System.out.println(data.get(0));
+    }
+
+    public static ArrayList<Double> readData(String filePath) {
+        ArrayList<Double> data = new ArrayList<Double>();
+
+        //read in data from file
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.add(Double.parseDouble(line));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("error: " + e);
+        } catch (IOException e) {
+            System.out.println("error: " + e);
+        }
+
+        return data;
     }
 }
